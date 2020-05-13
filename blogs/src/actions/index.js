cf168616e6397d733bf256d30bcd2bab169668b8
Annika Chauhan <annikac0711@gmail.com>
2020-05-13 12:34:40 +0100

    hooked up to redux thunk to produce the middleware function to handle the asynchronous request to the jsonplaceholder api endpoint, I have left the function in it's original state and also added the refactored version as they both have my notes on

--- blogs/src/actions/index.js
+++ blogs/src/actions/index.js
@@ -1,10 +1,20 @@
 import jsonPlaceholder from '../apis/jsonPlaceholder';
+import { dispatch } from 'redux-thunk';
 
-export const fetchPosts = async () => {
+//this one is the middleware so looks a bit different to other actions
+//with redux thunk you can now use async/await again, where it couldn't be used in vanilla redux
+//we don't need the getState for this function but it could be needed for others - you would pass getState as a parameter on the async function
+//this is the old layout
+// export const fetchPosts = () => {
+//     return async (dispatch, getState) => {
+//         const response = await jsonPlaceholder.get('/posts');
+
+//         dispatch({ type: 'FETCH_POSTS', payload: response })
+//     };
+// };
+
+//this is the new refactored layout that does the same as above but looks neater, it is simple a function returning a function
+export const fetchPosts = () => async dispatch => {
     const response = await jsonPlaceholder.get('/posts');
-    
-    return {
-        type: 'FETCH_POSTS', 
-        payload: response
-    }
-}
+    dispatch({ type: 'FETCH_POSTS', payload: response })
+};
